cddc607c20fbf1965ac1aafd4a965bcd5eca1014
Mateusz Burzynski <mateusz.burzynski@atos.net>
2018-11-03 10:27:30 +0100

    postman todoapi

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+
+let Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 1
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo};
